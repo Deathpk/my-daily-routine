b34e59407bbc6a8c778001cb759b465c3f3d8aa7
versianidev <bettercallmiguel@gmail.com>
2024-02-14 12:13:06 -0300

    New - updating routine

--- src/data/config.ts
+++ src/data/config.ts
@@ -11,11 +11,11 @@ export const config: ConfigType = {
   between your own quotes
   */
   quotes: [
-    // {
-    //   quote:
-    //     "Motivation is what gets you started. Habit is what keeps you going",
-    //   author: "Aristotle",
-    // },
+    {
+      quote:
+        "Motivation is what gets you started. Habit is what keeps you going",
+      author: "Aristotle",
+    },
   ],
 
   timetable: [
@@ -28,36 +28,36 @@ export const config: ConfigType = {
     },
     {
       name: "Morning Routine",
-      memo: "Meditation, Breakfast, Ride bike.",
+      memo: "Breakfast, Ride bike, 10 Pushups, Shower, Meditate, Check your goals",
       start: "06:00",
-      end: "07:30",
-      color: color.ITEM_COLOR_ORANGE,
+      end: "08:10",
+      color: color.ITEM_COLOR_YELLOW,
     },
     {
       name: "Coffee & Study",
-      memo: "Backend and architecture stuff",
-      start: "07:50",
-      end: "09:00",
+      memo: "Docker, Requirements Analysis, Design Patterns, System Design, PHP, Node, Livewire, Filament",
+      start: "08:10",
+      end: "09:40",
       color: color.ITEM_COLOR_CYAN,
     },
     {
       name: "Work on important tasks",
       memo: "Frelance or personal projects.",
-      start: "09:20",
-      end: "13:20",
+      start: "09:40",
+      end: "13:40",
       color: color.ITEM_COLOR_GRAY,
     },
     {
       name: "Lunch",
       memo: "Eat healthy food",
-      start: "13:21",
-      end: "14:30",
+      start: "13:40",
+      end: "14:40",
       color: color.ITEM_COLOR_CYAN,
     },
     {
       name: "Light work",
       memo: "Not important tasks",
-      start: "14:41",
+      start: "14:40",
       end: "18:50",
       color: color.ITEM_COLOR_ORANGE,
     },
@@ -76,8 +76,8 @@ export const config: ConfigType = {
       color: color.ITEM_COLOR_GRAY,
     },
     {
-      name: "Sleep",
-      memo: "No smartphone",
+      name: "Jornal and Sleep",
+      memo: "Jornaling first, No smartphone after It's done!",
       start: "22:00",
       end: "24:00",
       color: color.ITEM_COLOR_PURPLE,
